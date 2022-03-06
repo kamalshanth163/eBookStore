c5c2cd5a2ae8d3f4df0755c99724185bdd28cba7
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-03-06 12:01:24 +0530

    Initial commit

---
+++ CreateTablesScript.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [dbo].[books] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [title]        VARCHAR (50)  NULL,
+    [info]         VARCHAR (MAX) NULL,
+    [bookquantity] INT           NULL,
+    [price]        INT           NULL,
+    [imgfile]      VARCHAR (50)  NULL,
+    [cataid]       INT           NULL,
+    [author]       VARCHAR (50)  NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[orders] (
+    [id]        INT  IDENTITY (1, 1) NOT NULL,
+    [bookId]    INT  NOT NULL,
+    [userid]    INT  NULL,
+    [quantity]  INT  NULL,
+    [orderdate] DATE DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_orders] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
+CREATE TABLE [dbo].[users] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [name] VARCHAR (50) NULL,
+    [pass] VARCHAR (50) NULL,
+    [role] VARCHAR (50) NULL,
+    [email] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
