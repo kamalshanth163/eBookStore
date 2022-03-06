291819c17196dec339dacf2f474aebe8c03a0526
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-03-06 19:04:58 +0530

    Create, edit, display books

--- CreateTablesScript.sql
+++ CreateTablesScript.sql
@@ -1,29 +1,29 @@
-﻿CREATE TABLE [dbo].[books] (
+﻿CREATE TABLE [dbo].[Books] (
     [Id]           INT           IDENTITY (1, 1) NOT NULL,
-    [title]        VARCHAR (50)  NULL,
-    [info]         VARCHAR (MAX) NULL,
-    [bookquantity] INT           NULL,
-    [price]        INT           NULL,
-    [imgfile]      VARCHAR (50)  NULL,
-    [cataid]       INT           NULL,
-    [author]       VARCHAR (50)  NULL,
+    [Title]        VARCHAR (50)  NULL,
+    [Info]         VARCHAR (MAX) NULL,
+    [Bookquantity] INT           NULL,
+    [Price]        DECIMAL(19,4) NULL,
+    [Imgfile]      VARCHAR (50)  NULL,
+    [Cataid]       INT           NULL,
+    [Author]       VARCHAR (50)  NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
-CREATE TABLE [dbo].[orders] (
-    [id]        INT  IDENTITY (1, 1) NOT NULL,
-    [bookId]    INT  NOT NULL,
-    [userid]    INT  NULL,
-    [quantity]  INT  NULL,
-    [orderdate] DATE DEFAULT (getdate()) NULL,
-    CONSTRAINT [PK_orders] PRIMARY KEY CLUSTERED ([id] ASC)
+CREATE TABLE [dbo].[Orders] (
+    [Id]        INT  IDENTITY (1, 1) NOT NULL,
+    [BookId]    INT  NOT NULL,
+    [Userid]    INT  NULL,
+    [Quantity]  INT  NULL,
+    [Orderdate] DATE DEFAULT (getdate()) NULL,
+    CONSTRAINT [PK_orders] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
-CREATE TABLE [dbo].[users] (
+CREATE TABLE [dbo].[Users] (
     [Id]   INT          IDENTITY (1, 1) NOT NULL,
-    [name] VARCHAR (50) NULL,
-    [pass] VARCHAR (50) NULL,
-    [role] VARCHAR (50) NULL,
-    [email] VARCHAR (50) NULL,
+    [Name] VARCHAR (50) NULL,
+    [Pass] VARCHAR (50) NULL,
+    [Role] VARCHAR (50) NULL,
+    [Email] VARCHAR (50) NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
---
+++ eBookStore/Data/eBookStoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using eBookStore.Models;
+
+namespace eBookStore.Data
+{
+    public class eBookStoreContext : DbContext
+    {
+        public eBookStoreContext (DbContextOptions<eBookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
