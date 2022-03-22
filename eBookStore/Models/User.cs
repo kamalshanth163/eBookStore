0211f36fa9b03517d5f05b735a66c38c96f693ec
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-03-22 13:51:24 +0530

    Add user model

---
+++ eBookStore/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace eBookStore.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Pass { get; set; }
+        public string Role { get; set; }
+        public string Email { get; set; }
+    }
+}
