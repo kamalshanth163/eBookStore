4a5d4fbf6a8db33b3eaf867cba7d3ca4e9f5ab9f
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-03-31 13:59:46 +0530

    manage orders

--- eBookStore/Data/eBookStoreContext.cs
+++ eBookStore/Data/eBookStoreContext.cs
@@ -17,5 +17,7 @@ namespace eBookStore.Data
         public DbSet<Book> Books { get; set; }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
